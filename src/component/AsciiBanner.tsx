bfa99a7ba340c9bbca80324610ab50c00a467a55
Maulana Muhammad <maulllanamuhammad@gmail.com>
2025-06-15 17:05:11 +0700

    feat : ascii banner component

---
+++ src/component/AsciiBanner.tsx
@@ -0,0 +1,15 @@
+const AsciiBanner = () => {
+  return (
+    <pre className="whitespace-pre font-mono text-sm leading-tight text-green-400 overflow-x-auto block">
+      {`
+    __  _________                  
+   /  |/  /_  __/__  _________ ___ 
+  / /|_/ / / / / _ \\/ ___/ __ '__ \\ 
+ / /  / / / / /  __/ /  / / / / / /
+/_/  /_/ /_/  \\___/_/  /_/ /_/ /_/                         
+`}
+    </pre>
+  );
+};
+
+export default AsciiBanner;
