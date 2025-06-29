f66ea8bce31c8030e36604c84eff9e52f01b03f8
Maulana Muhammad <maulllanamuhammad@gmail.com>
2025-06-29 18:52:11 +0700

    feat : user interface

---
+++ src/interfaces/User.ts
@@ -0,0 +1,11 @@
+export interface User {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  isDeleted: boolean;
+  name: string;
+  title: string;
+  email: string;
+  phone: string;
+  summary: string;
+}
